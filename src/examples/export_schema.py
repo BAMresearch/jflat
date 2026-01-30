b72db6ec55c9264a6a17c1333bb4d4c9c96f949d
KoeppelSoftwareEngineer <MaximKoeppelPower@gmail.com>
2026-01-30 09:45:23 +0100

    Comments are Updated

--- src/examples/export_schema.py
+++ src/examples/export_schema.py
@@ -1,8 +1,11 @@
 from schema import Method
 import json
 
-"""File to create a json for "Ticket - schema_json.json" with model_json_schema()
-this can also be run in a terminal 
+"""
+File to create a JSON Schema (stored in `./examples/schema_json.json`) based on the example data model defined
+in `./examples/schema.py`. 
+
+This JSON Schema is produced by calling `Method.model_json_schema()`, where `Method` is defined in `schema.py`. 
 """
 
 schema = Method.model_json_schema()
